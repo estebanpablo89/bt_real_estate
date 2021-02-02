5602ed400676d90f929d3d04e0d5261141331caf
estebanpablo89 <estebanpablo89@gmail.com>
2021-02-01 19:05:58 -0500

    html

--- btre/settings.py
+++ btre/settings.py
@@ -121,4 +121,4 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
-STATICFILE_DIRS = [os.path.join(BASE_DIR, "btre/static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "btre/static")]
