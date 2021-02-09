5429164009bd131f88ea8abff0e5b7aca1f730a1
estebanpablo89 <estebanpablo89@gmail.com>
2021-02-09 11:58:45 -0500

    db conn

--- btre/settings.py
+++ btre/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = "btre.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "btredb",
+        "USER": "web",
+        "PASSWORD": "webuser",
+        "HOST": "localhost",  # Or an IP Address that your DB is hosted on
+        "PORT": "3306",
     }
 }
 
